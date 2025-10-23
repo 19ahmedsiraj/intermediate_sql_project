0ed118376441db3ab5d0857dfdc632602165188d
Siraj <19ahmedsiraj@gmail.com>
2025-10-23 23:52:29 +0530

    commit_2

--- Scripts/Script-1.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-date_trunc('month', ca.orderdate)::date year_month,
-SUM(total_net_revenue) AS revenue,
-COUNT(DISTINCT customerkey) AS total_customers,
-SUM(total_net_revenue)/COUNT(DISTINCT customerkey) AS customer_revenue
-FROM cohort_analysis ca 
-GROUP BY year_month
-ORDER BY year_month 
--- coalesce.sql
+++
@@ -1,9 +0,0 @@
-WITH total_sales AS (select customerkey,
-SUM(netprice*exchangerate*quantity) as net_revenue
-from sales
-group by customerkey)
-
-select
-AVG(net_revenue) as spending_customers,
-AVG(COALESCE(net_revenue,0)) as all_customers
-from customer c left join total_sales t on c.customerkey=t.customerkey
